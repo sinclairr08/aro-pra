ca56e3441b49e889781dcbd416d2ea56fe65eab7
sinclairr08 <ljhjoon00@gmail.com>
2025-05-06 19:39:08 +0900

    feat: add css style for header and footer

--- frontend/components/layout/Footer.tsx
+++ frontend/components/layout/Footer.tsx
@@ -1,12 +1,12 @@
 const Footer: React.FC = () => {
   return (
-    <div className="w-full fixed bottom-0 p-3 flex flex-col text-center text-xs bg-cyan-400 text-white z- 10">
+    <footer className="w-full fixed bottom-0 bg-gradient-to-r from-cyan-400/90 from-10% via-pink-200/30 via-50% to-[#332c30] p-3 flex flex-col text-center text-xs text-gray-800 z-10 shadow-lg">
       <span>© 2025. aro-pra</span>
       <span>
         이 페이지는 넥슨 게임즈의 공식 서비스가 아니며, 모든 리소스의 저작권은
         저작권자에게 있습니다
       </span>
-    </div>
+    </footer>
   );
 };
 
--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 const Header: React.FC = () => {
   return (
-    <div className="w-full fixed top-0 p-3 flex items-center text-center bg-cyan-400 text-white font-bold z-10">
+    <header className="w-full fixed top-0 p-3 flex items-center text-center bg-gradient-to-r from-cyan-400/90 from-10% via-pink-200/30 via-50% to-[#332c30] text-gray-800 font-bold z-10">
       <Link href="/">
         <span className="mx-4">HOME</span>
       </Link>
-    </div>
+    </header>
   );
 };
 
