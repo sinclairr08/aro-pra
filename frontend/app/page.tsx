2791c04a065295b9509f969ea950619698425aa1
sinclairr08 <ljhjoon00@gmail.com>
2025-05-09 23:11:19 +0900

    feat: add api fetching code at main page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
 import "@/app/globals.css";
 import Link from "next/link";
 import Image from "next/image";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 interface LinkCard {
   id: number;
@@ -8,18 +12,45 @@ interface LinkCard {
   url: string;
 }
 
-const linkCards: LinkCard[] = [
+const mocklLinkCards: LinkCard[] = [
   { id: 1, name: "Mn", url: "https://molunote.oopy.io" },
   { id: 2, name: "AI", url: "https://arona.ai" },
   { id: 3, name: "GONGSIKJP", url: "https://bluearchive.jp" },
   { id: 4, name: "GONGSIKKR", url: "https://forum.nexon.com/bluearchive" },
+  { id: 5, name: "SG", url: "http://schaledb.com" },
 ];
+
+const fetchLinkCards = async (): Promise<LinkCard[]> => {
+  const useApi = false;
+
+  if (useApi) {
+    try {
+      const response = await axios.get("/api/v1/links");
+      return response.data;
+    } catch (error) {
+      return mocklLinkCards;
+    }
+  }
+
+  return Promise.resolve(mocklLinkCards);
+};
 
 const getFaviconUrl = (url: string): string => {
   return `https://www.google.com/s2/favicons?domain=${url}&sz=64`;
 };
 
 export default function Home() {
+  const [linkCards, setLinkCards] = useState<LinkCard[]>([]);
+
+  useEffect(() => {
+    const getLinkCards = async () => {
+      const data = await fetchLinkCards();
+      setLinkCards(data);
+    };
+
+    getLinkCards();
+  }, []);
+
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {linkCards.map((card: LinkCard) => (
