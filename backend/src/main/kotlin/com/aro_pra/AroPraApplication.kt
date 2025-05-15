f4d3034684a9091978de8568b2edb1048fab9793
sinclairr08 <ljhjoon00@gmail.com>
2025-05-16 02:01:30 +0900

    config: add basic settings for kotlin spring backend

---
+++ backend/src/main/kotlin/com/aro_pra/AroPraApplication.kt
@@ -0,0 +1,11 @@
+package com.aro_pra
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AroPraApplication
+
+fun main(args: Array<String>) {
+	runApplication<AroPraApplication>(*args)
+}
---
+++ backend/src/test/kotlin/com/aro_pra/AroPraApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.aro_pra
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AroPraApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
