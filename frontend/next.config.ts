e6d5936c6a4a3847087d2849c7a7fca4a7b5ac6d
sinclairr08 <ljhjoon00@gmail.com>
2025-05-27 22:29:33 +0900

    feat: make fe page to use spring api

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -4,6 +4,15 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:8080/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
