f635675e95f532f8692c4ed0244c08a29f997727
sinclairr08 <ljhjoon00@gmail.com>
2025-06-18 23:16:55 +0900

    feat: add jwt libraries and jwt util class

--- backend/src/main/kotlin/com/aropra/controller/AdminController.kt
+++ backend/src/main/kotlin/com/aropra/controller/AdminController.kt
@@ -1,6 +1,35 @@
 package com.aropra.controller
 
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.*
+import java.security.Key
+import java.util.*
+
+@Component
+class JwtUtils {
+    @Value("\${app.jwt.secret}")
+    private lateinit var jwtSecret: String
+
+    @Value("\${app.jwt.expiration-ms}")
+    private var jwtExpiration: Long = 86400000L
+
+    private val key: Key by lazy {
+        Keys.hmacShaKeyFor(jwtSecret.toByteArray())
+    }
+
+    fun generateToken(): String =
+        Jwts
+            .builder()
+            .setSubject("admin")
+            .setIssuedAt(Date())
+            .setExpiration(Date(Date().time + jwtExpiration))
+            .signWith(key, SignatureAlgorithm.HS512)
+            .compact()
+}
 
 data class LoginRequest(
     val password: String,
