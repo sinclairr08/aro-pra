3e2f655950a09e8d2d6eb67c21295cf2ae55b1f5
sinclairr08 <ljhjoon00@gmail.com>
2025-07-08 21:53:03 +0900

    fix: fix wrong stop script

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -3,5 +3,5 @@
 FRONTEND_PORT=3000
 BACKEND_PORT=8080
 
-lsof -ti:$FRONTEND_PORT | xargs kill -9 2>/dev/null || true
-lsof -ti:$BACKEND_PORT | xargs kill -9 2>/dev/null || true
+fuser -k -n tcp $FRONTEND_PORT 2>/dev/null || true
+fuser -k -n tcp $BACKEND_PORT 2>/dev/null || true
