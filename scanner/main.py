d0e6c2312c24d252650c274418a4aeeefa3a9955
sinclairr08 <ljhjoon00@gmail.com>
2025-07-29 23:57:55 +0900

    feat: add basic download info function

--- scanner/main.py
+++ scanner/main.py
@@ -3,3 +3,17 @@ import requests
 
 print(UnityPy.__version__)
 print(requests.__version__)
+
+current_version = 'r52_uulekwyjhzir122lpbrw'
+BA_JP_VERSION_METADATA = f"https://yostar-serverinfo.bluearchiveyostar.com/{current_version}.json"
+
+current_version_assets_base_url = (
+    requests.get(BA_JP_VERSION_METADATA).json()
+    ["ConnectionGroups"][0]
+    ['OverrideConnectionGroups'][-1]
+    ['AddressablesCatalogUrlRoot']
+)
+
+BA_JP_ANDROID_BUNDLE_DOWNLOAD_INFO = f"{current_version_assets_base_url}/Android/bundleDownloadInfo.json"
+
+bundles_to_download = requests.get(BA_JP_ANDROID_BUNDLE_DOWNLOAD_INFO).json()['BundleFiles']
