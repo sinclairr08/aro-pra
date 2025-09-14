a0107093e281e67dfec2816877888eb451972d48
sinclairr08 <ljhjoon00@gmail.com>
2025-09-15 02:46:09 +0900

    refactor: make scanner to use same config file

--- scanner/config.py
+++ scanner/config.py
@@ -1,10 +1,17 @@
+import os
+
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class ScannerConfig(BaseSettings):
-    url: str
-    mongodb_uri: str
-    model_config = {"env_file": ".env.scanner"}
+    url: str = Field(alias="PATCH_URL")
+    mongodb_uri: str = Field(alias="SPRING_DATA_MONGODB_URI")
+
+    model_config = {
+        "env_file": f".env.{os.getenv('ENV', 'dev')}",
+        "extra": "ignore"
+    }
 
 
 CONFIG = ScannerConfig()
