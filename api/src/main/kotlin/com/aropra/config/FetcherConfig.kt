ceb354db7b53f9b37568422039fffabaec2afda0
sinclairr08 <ljhjoon00@gmail.com>
2025-10-26 07:08:39 +0900

    feat: add timeout

--- api/src/main/kotlin/com/aropra/config/FetcherConfig.kt
+++ api/src/main/kotlin/com/aropra/config/FetcherConfig.kt
@@ -2,10 +2,17 @@ package com.aropra.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import java.time.Duration
 
 @Configuration
 class FetcherConfig {
     @Bean
-    fun webClient(): WebClient = WebClient.create()
+    fun webClient(): WebClient {
+        val httpClient = HttpClient.create().responseTimeout(Duration.ofSeconds(30))
+
+        return WebClient.builder().clientConnector(ReactorClientHttpConnector(httpClient)).build()
+    }
 }
