8b51a8aaa1f6236d7f94bd25a4475cf8e419181f
sinclairr08 <ljhjoon00@gmail.com>
2025-11-04 10:23:52 +0900

    feat: add template for fetch post api

--- api/src/main/kotlin/com/aropra/fetcher/ExternalApiController.kt
+++ api/src/main/kotlin/com/aropra/fetcher/ExternalApiController.kt
@@ -5,11 +5,9 @@ import com.aropra.enum.Language
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.core.ParameterizedTypeReference
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.reactive.function.client.WebClient
+import java.net.URI
 
 @RestController
 @RequestMapping("/api/v1/fetch")
@@ -19,7 +17,7 @@ class ExternalApiController(
 ) {
     @GetMapping("/{lang}")
     @Cacheable("externalApi", key = "#lang") // TODO: move to service
-    fun fetchAndSaveAllData(
+    fun fetchAllData(
         @PathVariable lang: String,
     ): ResponseEntity<Map<String, ExternalStudent>> {
         val language = Language.fromString(lang) ?: return ResponseEntity.notFound().build()
@@ -34,5 +32,29 @@ class ExternalApiController(
 
         val topk = response.entries.take(10).associate { it.key to it.value }
         return ResponseEntity.ok(topk)
+    }
+
+    @PostMapping("/{lang}")
+    @Cacheable("externalApi", key = "#lang") // TODO: move to service
+    fun fetchAndSaveAllData(
+        @PathVariable lang: String,
+    ): ResponseEntity<Any> {
+        val language = Language.fromString(lang) ?: return ResponseEntity.notFound().build()
+        val url = properties.urlTemplate.replace("{{language}}", language.code)
+        val response =
+            wc
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(object : ParameterizedTypeReference<Map<String, ExternalStudent>>() {})
+                .block() ?: return ResponseEntity.notFound().build()
+
+        // Logic
+        // 1. find corresponding image. base image dir must be given
+        // 2. save to db with image path
+
+        // TODO: Fix location, use normal student api, not grouped
+        val location = URI.create("/api/v1/students/grouped/$language")
+        return ResponseEntity.created(location).build()
     }
 }
