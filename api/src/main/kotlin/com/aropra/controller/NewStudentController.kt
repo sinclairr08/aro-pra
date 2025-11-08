3cf88d007964041d1f65e2fe77e647cec7af4814
sinclairr08 <ljhjoon00@gmail.com>
2025-11-09 00:04:27 +0900

    refactor: separate to new student service

---
+++ api/src/main/kotlin/com/aropra/controller/NewStudentController.kt
@@ -0,0 +1,57 @@
+@file:Suppress("ktlint:standard:no-wildcard-imports")
+
+package com.aropra.controller
+
+import com.aropra.config.ExternalApiProperties
+import com.aropra.converter.toNewStudent
+import com.aropra.domain.ExternalStudent
+import com.aropra.enum.Language
+import com.aropra.service.NewStudentService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.URI
+import java.nio.file.Path
+import kotlin.io.path.exists
+
+@RestController
+@RequestMapping("/api/v1/new-student") // TODO: rename this
+class NewStudentController(
+    private val properties: ExternalApiProperties,
+    private val newStudentService: NewStudentService,
+) {
+    @PostMapping("/{lang}")
+    fun fetchAndSaveAllData(
+        @PathVariable lang: String,
+    ): ResponseEntity<Any> {
+        val language = Language.fromString(lang) ?: return ResponseEntity.notFound().build()
+        val response =
+            newStudentService.getStudentFromExternalSource(language) ?: return ResponseEntity
+                .status(
+                    HttpStatus.BAD_GATEWAY,
+                ).build()
+        val values = response.values.toList()
+        val newStudents =
+            values
+                .mapNotNull { value ->
+                    val path = getPath(value)
+                    if (path != null) value.toNewStudent(path) else null
+                }
+
+        // TODO: ADD LOGGING
+        val saved = newStudentService.createNewStudents(newStudents)
+        val location = URI.create("/api/v1/new-students/$lang")
+        return ResponseEntity.created(location).build()
+    }
+
+    fun getPath(externalStudent: ExternalStudent): Path? {
+        val baseDir = Path.of(properties.dataPath)
+        val path = baseDir.resolve(Path.of("Student_Portrait_${externalStudent.devName}.png"))
+        val backupPath = baseDir.resolve(Path.of("Student_Portrait_${externalStudent.name}.png"))
+
+        return path.takeIf { it.exists() } ?: backupPath.takeIf { it.exists() }
+    }
+}
--- api/src/main/kotlin/com/aropra/service/NewStudentService.kt
+++ api/src/main/kotlin/com/aropra/service/NewStudentService.kt
@@ -1,17 +1,39 @@
 package com.aropra.service
 
+import com.aropra.config.ExternalApiProperties
+import com.aropra.domain.ExternalStudent
 import com.aropra.domain.NewStudent
+import com.aropra.enum.Language
 import com.aropra.repository.NewStudentRepository
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.core.ParameterizedTypeReference
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.reactive.function.client.WebClient
 
 @Service
 class NewStudentService(
     private val newStudentRepository: NewStudentRepository,
+    private val wc: WebClient,
+    private val properties: ExternalApiProperties,
 ) {
     @Transactional
     fun createNewStudents(newStudents: List<NewStudent>): List<NewStudent> {
         if (newStudents.isEmpty()) return newStudents
         return newStudentRepository.saveAll(newStudents).toList()
+    }
+
+    @Cacheable("externalApi", key = "#language")
+    fun getStudentFromExternalSource(language: Language): Map<String, ExternalStudent>? {
+        val url = properties.urlTemplate.replace("{{language}}", language.code)
+        val response =
+            wc
+                .get()
+                .uri(url)
+                .retrieve()
+                .bodyToMono(object : ParameterizedTypeReference<Map<String, ExternalStudent>>() {})
+                .block()
+
+        return response
     }
 }
