429f3a7dc3cf5e05c3717160b62eb87de8b61de5
sinclairr08 <ljhjoon00@gmail.com>
2025-11-18 01:18:43 +0900

    feat: add index on mongodb

--- api/src/main/kotlin/com/aropra/domain/StudentImgCode.kt
+++ api/src/main/kotlin/com/aropra/domain/StudentImgCode.kt
@@ -1,11 +1,13 @@
 package com.aropra.domain
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 
 @Document(collection = "img_codes")
 data class StudentImgCode(
     @Id
     val id: String? = null,
+    @Indexed
     val code: String,
 )
--- api/src/main/kotlin/com/aropra/domain/Version.kt
+++ api/src/main/kotlin/com/aropra/domain/Version.kt
@@ -1,6 +1,7 @@
 package com.aropra.domain
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.time.LocalDateTime
 
@@ -11,5 +12,6 @@ data class Version(
     val version: String,
     val description: List<String>,
     val createdAt: LocalDateTime = LocalDateTime.now(),
+    @Indexed
     val updatedDate: String,
 )
