50998c36747cde843a9afc49653820dbe80fc6c0
sinclairr08 <ljhjoon00@gmail.com>
2025-11-23 02:23:33 +0900

    style: add python linting

--- scanner/config.py
+++ scanner/config.py
@@ -7,10 +7,7 @@ from pydantic_settings import BaseSettings
 class ScannerConfig(BaseSettings):
     mongodb_uri: str = Field(alias="SPRING_DATA_MONGODB_URI")
 
-    model_config = {
-        "env_file": f".env.{os.getenv('ENV', 'dev')}",
-        "extra": "ignore"
-    }
+    model_config = {"env_file": f".env.{os.getenv('ENV', 'dev')}", "extra": "ignore"}
 
 
 CONFIG = ScannerConfig()
